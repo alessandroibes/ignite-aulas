83308d04c6adfe0a2718c922f61403ff314fb9d0
Alessandro Vasconcelos de Oliveira <alessandrooliveira@pmenos.com.br>
2021-04-23 22:17:24 -0300

    Criado controller de Categoria

--- nodejs/04-rentalx/src/routes/categories.routes.ts
+++ nodejs/04-rentalx/src/routes/categories.routes.ts
@@ -1,19 +1,19 @@
 import { Router } from "express";
 
+import { CreateCategoyController } from "../modules/cars/controller/CreateCategoryController";
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
 
 const categoriesRoutes = Router();
+
 const categoriesRepository = new CategoriesRepository();
+const createCategoryService = new CreateCategoryService(categoriesRepository);
+const createCategoryController = new CreateCategoyController(
+  createCategoryService
+);
 
 categoriesRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({ name, description });
-
-  return response.status(201).send();
+  return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
